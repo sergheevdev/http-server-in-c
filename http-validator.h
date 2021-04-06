30faab8365644b36f020bcac28cf0a3b34cbbead
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-04-06 20:19:15 +0200

    Added http validator
    - The http validator will represent the "basic rules" specified in RFC2616

---
+++ http-validator.h
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+
+/* http-validator.h */
+#ifndef HTTP_VALIDATOR_H
+#define HTTP_VALIDATOR_H
+
+bool http_validator_is_space(char character);
+bool http_validator_is_eol(char character);
+bool http_validator_is_hex(char character);
+bool http_validator_is_space(char character);
+bool http_validator_is_separator(char character);
+bool http_validator_is_token(char character);
+
+#endif /* HTTP_VALIDATOR_H */
