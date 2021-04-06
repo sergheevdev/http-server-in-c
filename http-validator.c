dc87b1b7090a5e454733c2deef6a0d437ca2d294
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-04-06 23:24:39 +0200

    Added http validator implementation

---
+++ http-validator.c
@@ -0,0 +1,44 @@
+#include "http-validator.h"
+#include <ctype.h>
+
+bool http_validator_is_space(char character) {
+    return character == ' ' || character == '\t';
+}
+
+bool http_validator_is_eol(char character) {
+    return character == '\r' || character == '\n';
+}
+
+bool http_validator_is_hex(char character) {
+    return (character >= '0' && character <= '9') ||
+           (character >= 'a' && character <= 'f') ||
+           (character >= 'A' && character <= 'F');
+}
+
+bool http_validator_is_separator(char character) {
+    return character == ')' ||
+           character == '(' ||
+           character == '<' ||
+           character == '>' ||
+           character == '@' ||
+           character == ',' ||
+           character == ';' ||
+           character == ':' ||
+           character == '\\' ||
+           character == '"' ||
+           character == '/' ||
+           character == '[' ||
+           character == ']' ||
+           character == '?' ||
+           character == '=' ||
+           character == '{' ||
+           character == '}' ||
+           character == ' ' ||
+           character == '\t';
+}
+
+bool http_validator_is_token(char character) {
+    return http_validator_is_separator(character) == false &&
+           iscntrl(character) == false &&
+           character != 127;
+}
