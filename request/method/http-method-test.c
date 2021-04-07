6bfae25b2dacf4d80dc49339ab16a38d751bed25
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-04-07 21:05:18 +0200

    Refactored http method test
    - Added the "_test" prefix to all test methods for consistency with all test naming
    - Added dynamic memory allocation correct error handling

--- request/method/http-method-test.c
+++ request/method/http-method-test.c
@@ -10,16 +10,20 @@ void assert(int condition, char * message) {
     }
 }
 
-void http_method_ok() {
+void http_method_ok_test() {
     char * input = strdup("GET");
     char * http_method = http_method_from_string(input);
+    if(http_method == NULL) {
+        fprintf(stderr, "[http_method_not_ok] Fatal: Unable to allocate memory for the http_method variable");
+        exit(1);
+    }
     assert(http_method != NULL, "[http_method_ok] Expected: http_method != NULL");
     free(input);
     free(http_method);
     printf("Test http_method_ok passed!\n");
 }
 
-void http_method_not_ok() {
+void http_method_not_ok_test() {
     char * input = strdup("PATCH");
     char * http_method = http_method_from_string(input);
     assert(http_method == NULL, "[http_method_not_ok] Expected: http_method == NULL");
@@ -30,6 +34,6 @@ void http_method_not_ok() {
 
 int main() {
     fclose(stderr);
-    http_method_ok();
-    http_method_not_ok();
+    http_method_ok_test();
+    http_method_not_ok_test();
 }
