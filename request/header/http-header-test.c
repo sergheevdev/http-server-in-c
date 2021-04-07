35d1caede248a82cd06d81b02e00cf596b0065d8
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-04-07 21:21:30 +0200

    Replaced method hard-coded method name prefixes to "__func__"
    - Replaced hard-coded method prefixes to C99 pre-defined identifier "__func__"
    - The reason why we are doing this is to prevent human mistakes, i.e. writing the name of the function that we're not currently in

--- request/header/http-header-test.c
+++ request/header/http-header-test.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-void assert(int condition, char * message) {
+void assert(int condition, char * message, char * caller) {
     if(condition != 1) {
-        printf("%s\n", message);
+        printf("[%s] %s\n", caller, message);
         exit(1);
     }
 }
@@ -13,107 +13,107 @@ void assert(int condition, char * message) {
 void http_header_create_ok_test() {
     char * name = strdup("Accept");
     if(name == NULL) {
-        fprintf(stderr, "[http_header_create_ok_test] Fatal: Unable to allocate memory for the name variable");
+        fprintf(stderr, "[%s] Fatal: Unable to allocate memory for the name variable", __func__);
         exit(1);
     }
     char * value = strdup("text/plain");
     if(value == NULL) {
-        fprintf(stderr, "[http_header_create_ok_test] Fatal: Unable to allocate memory for the value variable");
+        fprintf(stderr, "[%s] Fatal: Unable to allocate memory for the value variable", __func__);
         exit(1);
     }
     HttpHeader * http_header = http_header_create(name, value);
-    assert(http_header != NULL, "[http_header_create_test_ok] Expected: http_header != NULL");
-    assert(name == http_header->name, "[http_header_create_test_ok] Expected: name == http_header->name");
-    assert(value == http_header->value, "[http_header_create_test_ok] Expected: name == http_header->name");
+    assert(http_header != NULL, "Expected: http_header != NULL", __func__);
+    assert(name == http_header->name, "Expected: name == http_header->name", __func__);
+    assert(value == http_header->value, "Expected: name == http_header->name", __func__);
     http_header_free(http_header);
-    printf("Test http_header_create_test_ok passed!\n");
+    printf("Test %s passed!\n", __func__);
 }
 
 void http_header_create_name_null_test() {
     char * name = NULL;
     char * value = strdup("text/plain");
     if(value == NULL) {
-        fprintf(stderr, "[http_header_create_name_null_test] Fatal: Unable to allocate memory for the value variable");
+        fprintf(stderr, "[%s] Fatal: Unable to allocate memory for the value variable", __func__);
         exit(1);
     }
     HttpHeader * http_header = http_header_create(name, value);
-    assert(http_header == NULL, "[http_header_create_test_name_null] Expected: http_header == NULL");
+    assert(http_header == NULL, "Expected: http_header == NULL", __func__);
     free(value);
-    printf("Test http_header_create_test_name_null passed!\n");
+    printf("Test %s passed!\n", __func__);
 }
 
 void http_header_create_name_invalid_test() {
     // Forced on purpose invalid header name containing invalid character
     char * name = strdup("\x01\x02\x03\x04");
     if(name == NULL) {
-        fprintf(stderr, "[http_header_create_name_invalid_test] Fatal: Unable to allocate memory for the name variable");
+        fprintf(stderr, "[%s] Fatal: Unable to allocate memory for the name variable", __func__);
         exit(1);
     }
     char * value = strdup("text/plain");
     if(value == NULL) {
-        fprintf(stderr, "[http_header_create_name_invalid_test] Fatal: Unable to allocate memory for the value variable");
+        fprintf(stderr, "[%s] Fatal: Unable to allocate memory for the value variable", __func__);
         exit(1);
     }
     HttpHeader * http_header = http_header_create(name, value);
-    assert(http_header == NULL, "[http_header_create_name_invalid_test] Expected: http_header == NULL");
+    assert(http_header == NULL, "Expected: http_header == NULL", __func__);
     free(name);
     free(value);
-    printf("Test http_header_create_name_invalid_test passed!\n");
+    printf("Test %s passed!\n", __func__);
 }
 
 void http_header_create_value_null_test() {
     char * name = strdup("Accept");
     if(name == NULL) {
-        fprintf(stderr, "[http_header_create_value_null_test] Fatal: Unable to allocate memory for the name variable");
+        fprintf(stderr, "[%s] Fatal: Unable to allocate memory for the name variable", __func__);
         exit(1);
     }
     char * value = NULL;
     HttpHeader * http_header = http_header_create(name, value);
-    assert(http_header == NULL, "[http_header_create_value_null_test] Expected: http_header == NULL");
+    assert(http_header == NULL, "Expected: http_header == NULL", __func__);
     free(name);
-    printf("Test http_header_create_test_value_null passed!\n");
+    printf("Test %s passed!\n", __func__);
 }
 
 void http_header_create_all_null_test() {
     char * name = NULL;
     char * value = NULL;
     HttpHeader * http_header = http_header_create(name, value);
-    assert(http_header == NULL, "[http_header_create_test_all_null] Expected: http_header == NULL");
-    printf("Test http_header_create_test_all_null passed!\n");
+    assert(http_header == NULL, "Expected: http_header == NULL", __func__);
+    printf("Test %s passed!\n", __func__);
 }
 
 void http_header_get_name_test() {
     char * name = strdup("Accept");
     if(name == NULL) {
-        fprintf(stderr, "[http_header_get_name_test] Fatal: Unable to allocate memory for the name variable");
+        fprintf(stderr, "[%s] Fatal: Unable to allocate memory for the name variable", __func__);
         exit(1);
     }
     char * value = strdup("text/plain");
     if(value == NULL) {
-        fprintf(stderr, "[http_header_get_name_test] Fatal: Unable to allocate memory for the value variable");
+        fprintf(stderr, "[%s] Fatal: Unable to allocate memory for the value variable", __func__);
         exit(1);
     }
     HttpHeader * http_header = http_header_create(name, value);
-    assert(strcmp(http_header->name, name) == 0, "[http_header_get_name_test] Expected: http_header->name === name");
+    assert(strcmp(http_header->name, name) == 0, "Expected: http_header->name === name", __func__);
     http_header_free(http_header);
-    printf("Test http_header_get_name_test passed!\n");
+    printf("Test %s passed!\n", __func__);
 }
 
 void http_header_get_value_test() {
     char * name = strdup("Accept");
     if(name == NULL) {
-        fprintf(stderr, "[http_header_get_value_test] Fatal: Unable to allocate memory for the name variable");
+        fprintf(stderr, "[%s] Fatal: Unable to allocate memory for the name variable", __func__);
         exit(1);
     }
     char * value = strdup("text/plain");
     if(value == NULL) {
-        fprintf(stderr, "[http_header_get_value_test] Fatal: Unable to allocate memory for the value variable");
+        fprintf(stderr, "[%s] Fatal: Unable to allocate memory for the value variable", __func__);
         exit(1);
     }
     HttpHeader * http_header = http_header_create(name, value);
-    assert(strcmp(http_header->value, value) == 0, "[http_header_get_value_test] Expected: http_header->value === value");
+    assert(strcmp(http_header->value, value) == 0, "Expected: http_header->value === value", __func__);
     http_header_free(http_header);
-    printf("Test http_header_get_value_test passed!\n");
+    printf("Test %s passed!\n", __func__);
 }
 
 int main() {
--- request/method/http-method-test.c
+++ request/method/http-method-test.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-void assert(int condition, char * message) {
+void assert(int condition, char * message, char * caller) {
     if(condition != 1) {
-        printf("%s\n", message);
+        printf("[%s] %s\n", caller, message);
         exit(1);
     }
 }
@@ -14,22 +14,22 @@ void http_method_ok_test() {
     char * input = strdup("GET");
     char * http_method = http_method_from_string(input);
     if(http_method == NULL) {
-        fprintf(stderr, "[http_method_not_ok] Fatal: Unable to allocate memory for the http_method variable");
+        fprintf(stderr, "[%s] Fatal: Unable to allocate memory for the http_method variable", __func__);
         exit(1);
     }
-    assert(http_method != NULL, "[http_method_ok] Expected: http_method != NULL");
+    assert(http_method != NULL, "[http_method_ok] Expected: http_method != NULL", __func__);
     free(input);
     free(http_method);
-    printf("Test http_method_ok passed!\n");
+    printf("Test %s passed!\n", __func__);
 }
 
 void http_method_not_ok_test() {
     char * input = strdup("PATCH");
     char * http_method = http_method_from_string(input);
-    assert(http_method == NULL, "[http_method_not_ok] Expected: http_method == NULL");
+    assert(http_method == NULL, "Expected: http_method == NULL", __func__);
     free(input);
     free(http_method);
-    printf("Test http_method_not_ok passed!\n");
+    printf("Test %s passed!\n", __func__);
 }
 
 int main() {
