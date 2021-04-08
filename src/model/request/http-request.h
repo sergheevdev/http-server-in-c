3200cbd59769a922171859e7d03ca8eac739bc9f
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-04-08 20:28:01 +0200

    Added http request

---
+++ src/model/request/http-request.h
@@ -0,0 +1,20 @@
+/* http-header.h */
+#ifndef HTTP_REQUEST_H
+#define HTTP_REQUEST_H
+
+struct request {
+    char * name;
+    char * value;
+};
+
+typedef struct request HttpRequest;
+
+HttpRequest * http_request_create(HttpMethod * method, HttpUri * uri, HttpVersion * version, HttpHeader * header, HttpBody * body);
+void http_request_free(HttpRequest * request);
+const HttpMethod * http_request_get_method(const HttpRequest * request);
+const HttpUri * http_request_get_uri(const HttpRequest * request);
+const HttpVersion * http_request_get_version(const HttpRequest * request);
+const HttpHeader * http_request_get_headers(const HttpRequest * request);
+const HttpBody * http_request_get_body(const HttpRequest * request);
+
+#endif /* HTTP_REQUEST_H */
