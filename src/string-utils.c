cbb5b960c356d11c84438116d3b1d182f1c23be9
Serghei Sergheev <47123121+sergheevdev@users.noreply.github.com>
2021-04-24 23:04:11 +0200

    Added tests

--- src/string-utils.c
+++ src/string-utils.c
@@ -14,6 +14,14 @@ struct string_builder {
 
 typedef struct string_builder StringBuilder;
 
+
+void assert(int condition, char * message) {
+    if(condition != 1) {
+        printf("%s\n", message);
+        exit(1);
+    }
+}
+
 /**
  * Creates a string builder with the default initial capacity and resize increment.
  *
@@ -34,6 +42,13 @@ StringBuilder * string_builder_create_default();
  * @return a new string builder
  */
 StringBuilder * string_builder_create(size_t initial_capacity, size_t resize_increment);
+
+/**
+ * Frees the string builder structure and all its dynamically allocated contents.
+ *
+ * @param string_builder the string builder that is about to be freed
+ */
+void string_builder_destroy(StringBuilder * string_builder);
 
 /**
  * Appends a character to the given string builder.
@@ -75,13 +90,25 @@ StringBuilder * string_builder_create_default() {
     return string_builder_create(DEFAULT_INITIAL_CAPACITY, DEFAULT_RESIZE_INCREMENT);
 }
 
+void string_builder_create_default_test() {
+    StringBuilder * string_builder = string_builder_create_default();
+    assert(string_builder != NULL, "Expected 'string_builder' not to be NULL");
+    assert(string_builder->built_chain != NULL, "Expected 'built_chain' not to be NULL");
+    assert(string_builder->initial_capacity == DEFAULT_INITIAL_CAPACITY, "Expected 'initial_capacity' to be equal to '128'");
+    assert(string_builder->used_capacity == 0, "Expected 'used_capacity' to be equal to '0'");
+    assert(string_builder->max_capacity == DEFAULT_INITIAL_CAPACITY, "Expected 'max_capacity' to be equal to '128'");
+    assert(string_builder->resize_increment == DEFAULT_RESIZE_INCREMENT, "Expected 'resize_increment' to be equal to '64'");
+    string_builder_destroy(string_builder);
+    printf("The test 'string_builder_create_default_test' passed successfully!\n");
+}
+
 StringBuilder * string_builder_create(size_t initial_capacity, size_t resize_increment) {
-    if(initial_capacity < 1) {
+    if(initial_capacity < 0) {
         fprintf(stderr, "The 'initial_capacity' provided to 'string_builder_create' "
                         "must be a positive integer, bigger or equal to '1'\n");
         return NULL;
     }
-    if(initial_capacity < 1) {
+    if(resize_increment < 1) {
         fprintf(stderr, "The 'resize_increment' provided to 'string_builder_create' "
                         "must be a positive integer, bigger or equal to '1'\n");
         return NULL;
@@ -103,6 +130,19 @@ StringBuilder * string_builder_create(size_t initial_capacity, size_t resize_inc
     string_builder->max_capacity = initial_capacity;
     string_builder->resize_increment = resize_increment;
     return string_builder;
+}
+
+void string_builder_create_using_invalid_resize_increment_test() {
+    StringBuilder * string_builder = string_builder_create(10, 0);
+    assert(string_builder == NULL, "Expected the builder to be NULL");
+    string_builder_destroy(string_builder);
+    printf("The test 'string_builder_create_using_invalid_resize_increment_test' passed successfully!\n");
+}
+
+void string_builder_destroy(StringBuilder * string_builder) {
+    if(string_builder == NULL) return;
+    if(string_builder->built_chain != NULL) free(string_builder->built_chain);
+    free(string_builder);
 }
 
 bool string_builder_ensure_capacity(StringBuilder * string_builder, size_t chars_amount) {
@@ -129,6 +169,17 @@ bool string_builder_ensure_capacity(StringBuilder * string_builder, size_t chars
     return true;
 }
 
+void string_builder_ensure_capacity_test() {
+    StringBuilder * string_builder = string_builder_create(5, 10);
+    for(int i = 0; i < 5; i++) {
+        string_builder_append(string_builder, 'A');
+    }
+    assert(string_builder->used_capacity == 5, "Expected used capacity to be equal to '5'");
+    assert(string_builder->max_capacity == 15, "Expected max capacity to be equal to '15'");
+    string_builder_destroy(string_builder);
+    printf("The test 'string_builder_ensure_capacity_test' passed successfully!\n");
+}
+
 bool string_builder_append(StringBuilder * string_builder, char character) {
     if(string_builder == NULL) {
         fprintf(stderr, "Trying to append a character to a NULL builder at 'string_builder_append'\n");
@@ -142,6 +193,20 @@ bool string_builder_append(StringBuilder * string_builder, char character) {
     (* current_position) = character;
     string_builder->used_capacity++;
     return true;
+}
+
+void string_builder_append_test() {
+    StringBuilder * string_builder = string_builder_create(2, 5);
+    string_builder_append(string_builder, 'A');
+    assert((* string_builder->built_chain) == 'A', "Expected first char to be equal to 'A'");
+    string_builder_append(string_builder, 'B');
+    assert((* (string_builder->built_chain + 1)) == 'B', "Expected second char to be equal to 'B'");
+    string_builder_append(string_builder, 'C');
+    assert((* (string_builder->built_chain + 2)) == 'C', "Expected third char to be equal to 'C'");
+    string_builder_append(string_builder, 'D');
+    assert((* (string_builder->built_chain + 3)) == 'D', "Expected fourth char to be equal to 'D'");
+    string_builder_destroy(string_builder);
+    printf("The test 'string_builder_append_test' passed successfully!\n");
 }
 
 bool string_builder_remove(StringBuilder * string_builder, size_t start_index, size_t stop_index) {
@@ -161,10 +226,11 @@ bool string_builder_remove(StringBuilder * string_builder, size_t start_index, s
         return false;
     }
     char * start = string_builder->built_chain + start_index;
-    char * next = string_builder->built_chain + stop_index;
-    size_t left_to_move = string_builder->used_capacity - stop_index;
-    string_builder->used_capacity -= left_to_move;
-    string_builder->built_chain -= (left_to_move - 1);
+    char * next = string_builder->built_chain + stop_index + 1;
+    size_t left_to_move = string_builder->used_capacity - (stop_index + 1);
+    // Adjust the capacity of the builder to the new one
+    string_builder->used_capacity -= (stop_index - start_index) + 1;
+    // Move the all the characters after the stop index to the left
     while(left_to_move != 0) {
         (* start) = (* next);
         start++;
@@ -172,6 +238,17 @@ bool string_builder_remove(StringBuilder * string_builder, size_t start_index, s
         left_to_move--;
     }
     return true;
+}
+
+void string_builder_remove_test() {
+    StringBuilder * string_builder = string_builder_create(2, 5);
+    string_builder_append(string_builder, 'A');
+    string_builder_append(string_builder, 'B');
+    string_builder_append(string_builder, 'C');
+    string_builder_append(string_builder, 'D');
+    string_builder_remove(string_builder, 1, 2);
+    assert((* string_builder->built_chain) == 'A', "Expected first char equal to 'A'");
+    assert((* (string_builder->built_chain + 1)) == 'D', "Expected second char equal to 'D'");
 }
 
 char * string_builder_result(StringBuilder * string_builder) {
@@ -213,275 +290,11 @@ char * string_builder_result_as_copy(StringBuilder * string_builder) {
 
 
 
-static const int BUFFER_SIZE = 128;
-
-
-/**
- * O (n+a*t) time | O (n+a*(-f+t)) space
- * - where n is the length of the original string
- * - where a is the number of "from" string ocurrences
- * - where f is the length of "from" string
- * - where t is the length of "to" string
- */
-char * string_replace_all(char * original, char * from, char * to) {
-
-    // Allocate the initial builder with its default capacity
-    char * builder = malloc(sizeof(char) * BUFFER_SIZE);
-    if(builder == NULL) {
-        fprintf(stderr, "Unable to allocate memory for the builder at string_replace_all\n");
-        return NULL;
-    }
-
-    char * builder_traverser = builder;   // Points to the next free position to append
-    size_t builder_used = 0;              // Counts the amount of appended characters to the builder
-    size_t builder_max = BUFFER_SIZE;     // Sets the initial maximum builder characters capacity
-    char * builder_potential = builder;   // Points to the next starting potential match char position
-    size_t builder_decrementor = 0;       // Counts the amount of possibly wrongly appended characters
-
-    char * original_traverser = original; // Points to the current traversal "original" character
-    char * from_traverser = from;         // Points to the character of the "from" chain that is being checked
-
-    // While didn't finish to iterate through the original chain
-    while((* original_traverser) != '\0') {
-
-        // If the "from_traverser" is at the end of the char sequence we have a match
-        if((* from_traverser) == '\0') {
-            // Move the current builder traversal position to the potential start and overwrite the wrongly appended chars
-            builder_traverser = builder_potential;
-            builder_used -= builder_decrementor;
-            // Append the "to" sequence to the builder and overwriting the previously wrongly appended chars
-            char * to_traverser = to;
-            while((* to_traverser) != '\0') {
-                // We ensure we have space for another char and we append the "to" sequence current character
-                if(builder_used == builder_max - 1) {
-                    // We resize the builder to BUFFER_SIZE more characters
-                    size_t new_size = builder_max + BUFFER_SIZE;
-                    // Ensure the resize was successfully completed
-                    char * new_builder = realloc(builder, sizeof(char) * new_size);
-                    if(new_builder == NULL) {
-                        fprintf(stderr, "Unable to reallocate memory of the new builder at string_replace_all\n");
-                        free(builder);
-                        // Prevent pointer deference
-                        builder_traverser = NULL;
-                        builder_potential = NULL;
-                        original_traverser = NULL;
-                        from_traverser = NULL;
-                        return NULL;
-                    }
-                    // Point the builder to its new pointer and change its size to the new size
-                    builder = new_builder;
-                    builder_max = new_size;
-                    // We update the old traversal pointer to its newly allocated one
-                    builder_traverser = builder;
-                    // We move the pointer to the position we were previously in
-                    builder_traverser = builder_traverser + builder_used;
-                }
-                (* builder_traverser) = (* to_traverser); // Append the "to" char value to the builder
-                builder_used++;                           // Increment the number of builder used slots
-                builder_traverser++;                      // Move to the next free position for the builder
-                to_traverser++;                           // Move to the next "to" sequence char to append
-            }
-            builder_potential = (builder_traverser);        // The next character might be a potential match
-            builder_decrementor = 0;                        // Reset the missed positions amount
-            from_traverser = from;                          // Restart the "from" traverser to check the next potential match from the beginning
-            original_traverser--;
-        }
-        // If there is a character match (we continue checking if matches the whole "to" chain)
-        else if((* original_traverser) == (* from_traverser)) {
-            // We ensure we have space for another char and we append the char (supposing there will not be a whole match)
-            if(builder_used == builder_max - 1) {
-                // We resize the builder to BUFFER_SIZE more characters
-                size_t new_size = builder_max + BUFFER_SIZE;
-                // Ensure the resize was successfully completed
-                char * new_builder = realloc(builder, sizeof(char) * new_size);
-                if(new_builder == NULL) {
-                    fprintf(stderr, "Unable to reallocate memory of the new builder at string_replace_all\n");
-                    free(builder);
-                    // Prevent pointer deference
-                    builder_traverser = NULL;
-                    builder_potential = NULL;
-                    original_traverser = NULL;
-                    from_traverser = NULL;
-                    return NULL;
-                }
-                // Point the builder to its new pointer and change its size to the new size
-                builder = new_builder;
-                builder_max = new_size;
-                // We update the old traversal pointer to its newly allocated one
-                builder_traverser = builder;
-                // We move the pointer to the position we were previously in
-                builder_traverser = builder_traverser + builder_used;
-            }
-            (* builder_traverser) = (* original_traverser); // Append the "original" char value to the builder
-            builder_used++;                                 // Increment the number of builder used slots
-            builder_traverser++;                            // Move to the next free position for the builder
-            builder_decrementor++;                          // If we are wrong and it is a match we store the amount of missed positions
-            from_traverser++;                               // Move to next position to check if the following chars also match
-        }
-        // The chain does not match so we were right so we can reset the amount of missed positions and the potential match
-        else {
-
-            // We ensure we have space for another char and we append the non-matching char which is part of the string
-            if(builder_used == builder_max - 1) {
-                // We resize the builder to BUFFER_SIZE more characters
-                size_t new_size = builder_max + BUFFER_SIZE;
-                // Ensure the resize was successfully completed
-                char * new_builder = realloc(builder, sizeof(char) * new_size);
-                if(new_builder == NULL) {
-                    fprintf(stderr, "Unable to reallocate memory of the new builder at string_replace_all\n");
-                    free(builder);
-                    // Prevent pointer deference
-                    builder_traverser = NULL;
-                    builder_potential = NULL;
-                    original_traverser = NULL;
-                    from_traverser = NULL;
-                    return NULL;
-                }
-                // Point the builder to its new pointer and change its size to the new size
-                builder = new_builder;
-                builder_max = new_size;
-                // We update the old traversal pointer to its newly allocated one
-                builder_traverser = builder;
-                // We move the pointer to the position we were previously in
-                builder_traverser = builder_traverser + builder_used;
-            }
-            (* builder_traverser) = (* original_traverser); // Append the "original" char value to the builder
-            builder_used++;                                 // Increment the number of builder used slots
-            builder_traverser++;                            // Move to the next free position for the builder
-
-            builder_potential = (builder_traverser);        // The next character might be a potential match
-            builder_decrementor = 0;                        // Reset the missed positions amount
-            from_traverser = from;                          // Restart the "from" traverser to check the next potential match from the beginning
-        }
-        original_traverser++;
-    }
-    (* builder_traverser) = '\0';
-    builder_used++;
-    // If the space used is bigged than needed we shorten the chain
-    if(builder_max > builder_used) {
-        char * new_builder = realloc(builder, sizeof(char) * builder_used);
-        if(new_builder == NULL) {
-            fprintf(stderr, "Unable to reallocate memory of the shortened builder at string_replace_all\n");
-            free(builder);
-            // Prevent pointer deference
-            builder_traverser = NULL;
-            builder_potential = NULL;
-            original_traverser = NULL;
-            from_traverser = NULL;
-            return NULL;
-        }
-        builder = new_builder;
-    }
-
-    // Prevent pointer deference
-    builder_traverser = NULL;
-    builder_potential = NULL;
-    original_traverser = NULL;
-    from_traverser = NULL;
-
-    // Return the built result
-    return builder;
-}
-
-struct test {
-    char * original;
-    char * from;
-    char * to;
-    char * expected;
-};
-
-typedef struct test Test;
-
-Test tests[] = {
-    {
-        "HeLLo darkness my oLd friend",
-        "L",
-        "l",
-        "Hello darkness my old friend"
-    },
-    {
-        "Hello {name}! Are you feeling good today? I hope yes, {name}!",
-        "{name}",
-        "Emilie",
-        "Hello Emilie! Are you feeling good today? I hope yes, Emilie!"
-    },
-    {
-        "Let's try to remove a character",
-        "e",
-        "",
-        "Lt's try to rmov a charactr"
-    },
-    {
-        "111222333222111",
-        "2",
-        "--++*++--",
-        "111--++*++----++*++----++*++--333--++*++----++*++----++*++--111"
-    },
-    {
-        "{ 'country': '{country}', 'max_ping': '35' }",
-        "{country}",
-        "Spain",
-        "{ 'country': 'Spain', 'max_ping': '35' }"
-    },
-    {
-        "",
-        "{action}",
-        "Perform",
-        ""
-    },
-    {
-        "",
-        "",
-        "",
-        ""
-    },
-    {
-        "aaaaa aaaaa aaaaa",
-        "aa",
-        "bb",
-        "bbbba bbbba bbbba"
-    },
-    {
-        "ababa ababa ababa",
-        "aba",
-        "X",
-        "Xba Xba Xba",
-    },
-    {
-        "{ 'restart': '%value%' }",
-        "%value%",
-        "true",
-        "{ 'restart': 'true' }"
-    }
-};
-
 int main() {
-
-    int size = sizeof(tests) / sizeof(tests[0]);
-    for(int i = 0; i < size; i++) {
-        Test current_test = tests[i];
-        char * original = strdup(current_test.original);
-        char * from = strdup(current_test.from);
-        char * to = strdup(current_test.to);
-        char * result = string_replace_all(original, from, to);
-        if(result != NULL) {
-            if(strcmp(result, current_test.expected) != 0) {
-                fprintf(stderr, "Assertion failed\n");
-                printf("Given: %s but expected: %s\n", result, current_test.expected);
-                exit(1);
-            }
-            free(result);
-        } else {
-            free(original);
-            free(from);
-            free(to);
-            fprintf(stderr, "Error\n");
-            exit(1);
-        }
-        free(original);
-        free(from);
-        free(to);
-    }
-    printf("All tests passed successfully");
-
+    fclose(stderr);
+    string_builder_create_default_test();
+    string_builder_create_using_invalid_resize_increment_test();
+    string_builder_ensure_capacity_test();
+    string_builder_append_test();
+    string_builder_remove_test();
 }
